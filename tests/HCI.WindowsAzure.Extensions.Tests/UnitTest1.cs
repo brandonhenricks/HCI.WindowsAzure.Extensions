1de048a8042c0dfcc1b1867df3f3d90114bb9edd
Brandon Henricks <brandon.henricks1984@gmail.com>
2020-03-10 10:23:45 -0400

    chore: Initial Commit

---
+++ tests/HCI.WindowsAzure.Extensions.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HCI.WindowsAzure.Extensions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
